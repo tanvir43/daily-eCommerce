0761bd1d5b8ebf25d8e169fe8de4a18cf049f413
tanvir43 <raihncse43@gmail.com>
2020-04-16 10:55:17 +0600

    modified product detail api endpoint

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
         # path('product-detail/<int:pk>/', views.product_detail),
         path('list/', views.ProductListAPIView.as_view(), name='list'),
         path('create/', views.ProductCreateAPIView.as_view(), name='create'),
-        path('<str:slug>/', views.ProductDetailAPIView.as_view(), name='detail'),
+        path('detail/<str:slug>/', views.ProductDetailAPIView.as_view(), name='detail'),
         path('edit/<int:pk>/', views.ProductUpdateAPIView.as_view(), name='update'),
         path('delete/<int:pk>/', views.ProductDetailAPIView.as_view(), name='delete'),
 ]

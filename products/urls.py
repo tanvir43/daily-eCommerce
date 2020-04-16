e3cf602d131d807a75433587190daffc25445c00
tanvir43 <raihncse43@gmail.com>
2020-04-16 20:49:36 +0600

    update api endpoints

--- products/urls.py
+++ products/urls.py
@@ -4,9 +4,9 @@ from products import views
 
 urlpatterns = [
         # path('product-detail/<int:pk>/', views.product_detail),
-        path('list/', views.ProductListAPIView.as_view(), name='list'),
-        path('create/', views.ProductCreateAPIView.as_view(), name='create'),
-        path('detail/<str:slug>/', views.ProductDetailAPIView.as_view(), name='detail'),
-        path('edit/<int:pk>/', views.ProductUpdateAPIView.as_view(), name='update'),
-        path('delete/<int:pk>/', views.ProductDetailAPIView.as_view(), name='delete'),
+        path('products/list/', views.ProductListAPIView.as_view(), name='list'),
+        path('products/create/', views.ProductCreateAPIView.as_view(), name='create'),
+        path('products/<str:slug>/detail/', views.ProductDetailAPIView.as_view(), name='detail'),
+        path('products/<int:pk>/edit/', views.ProductUpdateAPIView.as_view(), name='update'),
+        path('products/<int:pk>/delete/', views.ProductDetailAPIView.as_view(), name='delete'),
 ]

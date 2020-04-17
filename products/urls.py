5f2207b96fb71034a40e174952cc1dcbe25e415d
tanvir43 <raihncse43@gmail.com>
2020-04-17 21:51:56 +0600

    import Category related serializers and Category model, added CategoryList and CategoryDetail views

--- products/urls.py
+++ products/urls.py
@@ -4,9 +4,11 @@ from products import views
 
 urlpatterns = [
         # path('product-detail/<int:pk>/', views.product_detail),
-        path('products/list/', views.ProductListAPIView.as_view(), name='list'),
-        path('products/create/', views.ProductCreateAPIView.as_view(), name='create'),
-        path('products/<str:slug>/detail/', views.ProductDetailAPIView.as_view(), name='detail'),
-        path('products/<int:pk>/edit/', views.ProductUpdateAPIView.as_view(), name='update'),
-        path('products/<int:pk>/delete/', views.ProductDetailAPIView.as_view(), name='delete'),
+        path('category/list', views.CategoryListAPIView.as_view(), name='category-list'),
+        path('category/<int:pk>/detail', views.CategoryDetailAPIView.as_view(), name='category-detail'),
+        path('products/list/', views.ProductListAPIView.as_view(), name='product-list'),
+        path('products/create/', views.ProductCreateAPIView.as_view(), name='product-create'),
+        path('products/<str:slug>/detail/', views.ProductDetailAPIView.as_view(), name='product-detail'),
+        path('products/<int:pk>/edit/', views.ProductUpdateAPIView.as_view(), name='product-update'),
+        path('products/<int:pk>/delete/', views.ProductDetailAPIView.as_view(), name='product-delete'),
 ]

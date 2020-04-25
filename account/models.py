3b9476f9a7a70e336495d3a19015ed3ad0c1c1fb
tanvir43 <raihncse43@gmail.com>
2020-04-25 17:45:04 +0600

    account app added

---
+++ account/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.forms.models import model_to_dict
+
+from django_countries.fields import Country, CountryField
+from phonenumber_field.modelfields import PhoneNumber, PhoneNumberField
+
+# Create your models here.
+
+
+class Address(models.Model):
+    first_name = models.CharField(max_length=256, blank=True)
+    last_name = models.CharField(max_length=256, blank=True)
+    company_name = models.CharField(max_length=256, blank=True)
+    street_address_1 = models.CharField(max_length=256, blank=True)
+    street_address_2 = models.CharField(max_length=256, blank=True)
+    city = models.CharField(max_length=256, blank=True)
+    city_area = models.CharField(max_length=128, blank=True)
+    postal_code = models.CharField(max_length=20, blank=True)
+    country = CountryField()
+    country_area = models.CharField(max_length=128, blank=True)
+    phone = PhoneNumberField(blank=True, default="")
+
+    # objects = AddressQueryset.as_manager()
+
+    class Meta:
+        ordering = ("pk",)
+
+    @property
+    def full_name(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+    def __str__(self):
+        if self.company_name:
+            return "%s - %s" % (self.company_name, self.full_name)
+        return self.full_name
+
+    def __eq__(self, other):
+        if not isinstance(other, Address):
+            return False
+        return self.as_data() == other.as_data()
+
+    __hash__ = models.Model.__hash__
+
+    def as_data(self):
+        """Return the address as a dict suitable for passing as kwargs.
+
+        Result does not contain the primary key or an associated user.
+        """
+        data = model_to_dict(self, exclude=["id", "user"])
+        if isinstance(data["country"], Country):
+            data["country"] = data["country"].code
+        if isinstance(data["phone"], PhoneNumber):
+            data["phone"] = data["phone"].as_e164
+        return data
+
+    def get_copy(self):
+        """Return a new instance of the same address."""
+        return Address.objects.create(**self.as_data())

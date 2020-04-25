14e3f32f91fd120fe7c584cccc64ae5d43336016
tanvir43 <raihncse43@gmail.com>
2020-04-25 19:54:53 +0600

    adjust urls as 'slug' using as a lookup field

--- products/urls.py
+++ products/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
         # path('product-detail/<int:pk>/', views.product_detail),
         path('category/list', views.CategoryListAPIView.as_view(), name='category-list'),
         path('category/create', views.CategoryCreateAPIView.as_view(), name='category-create'),
-        path('category/<int:pk>/detail', views.CategoryDetailAPIView.as_view(), name='category-detail'),
+        path('category/<int:slug>/detail', views.CategoryDetailAPIView.as_view(), name='category-detail'),
         path('products/list', views.ProductListAPIView.as_view(), name='product-list'),
         path('products/create', views.ProductCreateAPIView.as_view(), name='product-create'),
         path('products/<str:slug>/detail', views.ProductDetailAPIView.as_view(), name='product-detail'),
-        path('products/<int:pk>/edit', views.ProductUpdateAPIView.as_view(), name='product-update'),
-        path('products/<int:pk>/delete', views.ProductDetailAPIView.as_view(), name='product-delete'),
+        path('products/<int:slug>/edit', views.ProductUpdateAPIView.as_view(), name='product-update'),
+        path('products/<int:slug>/delete', views.ProductDetailAPIView.as_view(), name='product-delete'),
 ]

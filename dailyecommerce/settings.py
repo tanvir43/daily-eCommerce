23ee6092321a3d20d7fa5dbc4cda83d50a32af32
tanvir43 <raihncse43@gmail.com>
2020-04-26 16:00:27 +0600

    remove comment tag on  admin contrib

--- dailyecommerce/settings.py
+++ dailyecommerce/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['dailyecommerce.herokuapp.com','localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',

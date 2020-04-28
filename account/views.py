4e80527e33a1b7c036d7f7554f46cf72f105a6c7
tanvir43 <raihncse43@gmail.com>
2020-04-28 15:49:35 +0600

    added UserRegistrationAPIView

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+    )
+from rest_framework import status, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
-# Create your views here.
+from account.models import Role, User, UserProfile
+from account.serializers import (
+    UserRegistrationSerializer,
+    )
+
+
+class UserRegistrationAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+

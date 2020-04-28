e02f4604e47a9460c3c487491cfcc623a323b159
tanvir43 <raihncse43@gmail.com>
2020-04-28 15:56:47 +0600

    added root url for account

---
+++ account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from account import views
+
+urlpatterns = [
+        path('category/create', views.UserRegistrationAPIView.as_view(), name='user-create'),
+        # path('category/<str:slug>/detail', views.CategoryDetailAPIView.as_view(), name='category-detail'),
+        # path('products/list', views.ProductListAPIView.as_view(), name='product-list'),
+        # path('products/create', views.ProductCreateAPIView.as_view(), name='product-create'),
+        # path('products/<str:slug>/detail', views.ProductDetailAPIView.as_view(), name='product-detail'),
+        # path('products/<str:slug>/edit', views.ProductUpdateAPIView.as_view(), name='product-update'),
+        # path('products/<str:slug>/delete', views.ProductDetailAPIView.as_view(), name='product-delete'),
+]

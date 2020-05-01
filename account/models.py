06c40342d38d082ee2e7358a1ed1cf7a60ffa9a6
tanvir43 <raihncse43@gmail.com>
2020-05-01 12:12:57 +0600

    modified user model as removed phone number field

--- account/models.py
+++ account/models.py
@@ -1,6 +1,6 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser, Group
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, Group, BaseUserManager
 from django.forms.models import model_to_dict
 
 from django_countries.fields import Country, CountryField
@@ -37,11 +37,43 @@ class Role(models.Model):
         return self.name
 
 
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
     groups = models.ForeignKey(Group, null=True, blank=True, on_delete=models.CASCADE)
     roles = models.ManyToManyField(Role, related_name='roles')
     username = models.CharField(max_length=100, blank=True, null=True)
-    phone = PhoneNumberField()
+    # phone = PhoneNumberField()
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
@@ -49,6 +81,7 @@ class User(AbstractUser):
         )
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email

c1b52b30003381f06be458188f2ae00b1105ae3f
tanvir43 <raihncse43@gmail.com>
2020-05-06 19:45:16 +0600

    remove extra codes

--- account/views.py
+++ account/views.py
@@ -9,11 +9,29 @@ from rest_framework.generics import (
 from rest_framework import status, permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-from account.models import Role, User, UserProfile
+from account.models import Role, User
 from account.serializers import (
     UserRegistrationSerializer,
+    UserLoginSerializer,
     )
+
+class UserLoginAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        status_code = status.HTTP_200_OK
+        response = {
+            'success': 'True',
+            'status code': status.HTTP_200_OK,
+            'message': 'User logged in  successfully',
+            'token': serializer.data['token'],
+        }
+        return Response(response, status=status_code)
 
 
 class UserRegistrationAPIView(CreateAPIView):

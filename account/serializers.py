dbd63ec7d5e79e5b5697271339795399dc6ad7ea
tanvir43 <raihncse43@gmail.com>
2020-05-06 19:45:41 +0600

    remove extra code and serializer class

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,14 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+
 from rest_framework import serializers
 from rest_framework.serializers import SerializerMethodField
 from rest_framework.relations import PrimaryKeyRelatedField
+
+from rest_framework_jwt.settings import api_settings
+
+JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
 
 from .models import Role, User
 
@@ -42,6 +50,40 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         if obj.groups:
             return obj.groups.name
         return None
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get('email', None)
+        password = data.get('password', None)
+        try:
+            user = authenticate(email=email, password=password)
+        except User.DoesNotExist:
+            raise serializers.ValidationError(
+                'User with given email and password does not exists'
+            )
+
+        # if user in None:
+        #     raise serializers.ValidationError(
+        #         'A user with this email and password is not found.'
+        #     )
+        # try:
+        payload = JWT_PAYLOAD_HANDLER(user)
+        jwt_token = JWT_ENCODE_HANDLER(payload)
+        update_last_login(None, user)
+        # except User.DoesNotExist:
+        #     raise serializers.ValidationError(
+        #         'User with given email and password does not exists'
+        #     )
+        return {
+            'email': user.email,
+            'token': jwt_token
+        }
+
 
     # def get_role_name(self, obj):
     #     names = []

5b3b6ef09eb588e575eec1ff3773cf964b4eccc8
tanvir43 <raihncse43@gmail.com>
2020-05-08 16:28:27 +0600

    added role,group and user login api view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import Group
+
 from rest_framework.generics import (
     CreateAPIView,
     ListAPIView,
@@ -6,47 +8,73 @@ from rest_framework.generics import (
     UpdateAPIView,
     DestroyAPIView
     )
+from rest_framework.views import APIView
 from rest_framework import status, permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from account.models import Role, User
 from account.serializers import (
     UserRegistrationSerializer,
     UserLoginSerializer,
+    UserRoleSerializer,
+    UserGroupSerializer
     )
+
 
 class UserLoginAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserLoginSerializer
+    permission_classes = (AllowAny,)
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        data = self.request.data
+        serializer = self.serializer_class(data=data)
         serializer.is_valid(raise_exception=True)
-        status_code = status.HTTP_200_OK
-        response = {
-            'success': 'True',
-            'status code': status.HTTP_200_OK,
-            'message': 'User logged in  successfully',
-            'token': serializer.data['token'],
-        }
-        return Response(response, status=status_code)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # def post(self, request):
+    #     serializer = self.serializer_class(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     status_code = status.HTTP_200_OK
+    #     response = {
+    #         'success': 'True',
+    #         'status code': status.HTTP_200_OK,
+    #         'message': 'User logged in  successfully',
+    #         'token': serializer.data['token'],
+    #     }
+    #     return Response(response, status=status_code)
 
 
-class UserRegistrationAPIView(CreateAPIView):
-    queryset = User.objects.all()
+class UserRegistrationAPIView(APIView):
+    permission_classes = (AllowAny,)
     serializer_class = UserRegistrationSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        print("DATA SHAK", request.data)
+        data = request.data
+        serializer = self.serializer_class(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         status_code = status.HTTP_201_CREATED
-        response = {
-            'success': 'True',
-            'status_code': status_code,
-            'message': 'Successfully registered'
-        }
-        return Response(response, status=status_code)
+        # response = {
+        #     'success': 'True',
+        #     'status_code': status_code,
+        #     'message': 'Successfully registered'
+        # }
+        return Response(serializer.data, status=status_code)
+
+
+class UserRoleCreateAPIView(CreateAPIView):
+    queryset = Role.objects.all()
+    serializer_class = UserRoleSerializer
+
+
+class UserGroupCreateAPIView(CreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = UserGroupSerializer
+
 

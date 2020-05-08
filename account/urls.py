cf2a93102fdb0f6549be16661ba360999ea29ec2
tanvir43 <raihncse43@gmail.com>
2020-05-08 16:26:47 +0600

    added endpoint for role create api and login api

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,8 @@ from account import views
 urlpatterns = [
         path('user/signup', views.UserRegistrationAPIView.as_view(), name='user-signup'),
         path('user/login', views.UserLoginAPIView.as_view(), name='user-login'),
+        path('user/role/create', views.UserRoleCreateAPIView.as_view(), name='user-role-create'),
+        # path('user/group/create', views.UserGroupCreateAPIView.as_view(), name='user-group-create'),
         # path('category/<str:slug>/detail', views.CategoryDetailAPIView.as_view(), name='category-detail'),
         # path('products/list', views.ProductListAPIView.as_view(), name='product-list'),
         # path('products/create', views.ProductCreateAPIView.as_view(), name='product-create'),

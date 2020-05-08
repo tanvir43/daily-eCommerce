e677fdabe9e4cf90c04a2bd6fe8b3402aebd5107
tanvir43 <raihncse43@gmail.com>
2020-05-08 16:25:46 +0600

    added login serializer and modified user registration serializer

--- account/serializers.py
+++ account/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import update_last_login
+from django.contrib.auth.models import Group, update_last_login ,Permission
+from django.contrib.auth.hashers import make_password
 
 from rest_framework import serializers
 from rest_framework.serializers import SerializerMethodField
@@ -13,7 +14,8 @@ JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
 from .models import Role, User
 
 
-class UserRoleSerializer(PrimaryKeyRelatedField, serializers.ModelSerializer):
+class UserRoleSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
 
     # def to_representation(self, value):
     #     return value.name
@@ -21,15 +23,85 @@ class UserRoleSerializer(PrimaryKeyRelatedField, serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = (
-            'name'
+            'id',
+            'name',
         )
 
 
+class UserGroupSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Group
+        fields = (
+            'id',
+            'name',
+        )
+
+
+class UserRoleNestedSerializer(PrimaryKeyRelatedField, serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+
+    # def to_representation(self, value):
+    #     return value.name
+
+    class Meta:
+        model = Role
+        fields = (
+            'id',
+            'name',
+        )
+
+
+class UserGroupNestedSerializer(PrimaryKeyRelatedField, serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Group
+        fields = (
+            'id',
+            'name',
+        )
+
+
+class PermissionSerializer(PrimaryKeyRelatedField, serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Permission
+        fields = '__all__'
+
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    # profile = UserProfileSerializer(required=False)
-    # role = serializers.PrimaryKeyRelatedField(queryset=Role.objects.all(), many=True)
-    roles = UserRoleSerializer(many=True, queryset=Role.objects.all())
-    group_name = SerializerMethodField()
+    """
+    Serializers registration requests and creates a new user.
+    """
+
+    # Ensure passwords are at least 8 characters long, no longer than 128
+    # characters, and can not be read by the client.
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    # The client should not be able to send a token along with a registration
+    # request. Making `token` read-only handles that for us.
+    token = serializers.CharField(read_only=True)
+
+    roles = UserRoleNestedSerializer(
+        many=True,
+        queryset=Role.objects.all()
+    )
+    groups = UserGroupNestedSerializer(
+        many=True,
+        queryset=Group.objects.all()
+    )
+    # permission = PermissionSerializer(
+    #     many=True,
+    #     queryset= Permission.objects.all()
+    # )
+
     # role_name = SerializerMethodField()
 
     class Meta:
@@ -37,51 +109,124 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = (
             'groups',
             'roles',
+            # 'permission',
             'username',
             'phone',
             'email',
             'password',
-            'group_name',
-            # 'role_name'
+            'token',
         )
-        extra_kwargs = {'password': {'write_only': True}}
 
-    def get_group_name(self, obj):
-        if obj.groups:
-            return obj.groups.name
-        return None
+    def validate_password(self, value: str) -> str:
+        """
+        Hash value passed by user.
+
+        :param value: password of a user
+        :return: a hashed version of the password
+        """
+        return make_password(value)
+
+    # def create(self, validated_data):
+    #     print("VL DATA", validated_data)
+    #     # print('group data rrr', validated_data.pop('groups'))
+    #     groups = validated_data.pop('groups')
+    #     roles = validated_data.pop('roles')
+    #
+    #     password = validated_data.pop('password')
+    #     # permission = Permission.objects.get(name=validated_data.pop('permission', None))
+    #     # groups = Group.objects.get(name=validated_data.pop('groups', None))
+    #     # roles = Role.objects.get(name=validated_data.pop('roles', None))
+    #     user = User.objects.create(
+    #         username=validated_data['username'],
+    #         email=validated_data['email'],
+    #         phone=validated_data['phone'],
+    #         # passsword=validated_data['password']
+    #     )
+    #     if groups:
+    #         user.groups.add(groups)
+    #     if roles:
+    #         user.roles.add(roles)
+    #     # if permission:
+    #     #     user.has_perm(permission)
+    #     # if password is not None:
+    #     user.set_password(password)
+    #     user.save()
+    #
+    #     return user
+        # extra_kwargs = {'password': {'write_only': True}}
+    # def create(self, validated_data):
+    #     return User.objects.create_user(**validated_data)
+
+    # def get_group_name(self, obj):
+    #     if obj.groups:
+    #         return obj.groups.name
+    #     return None
 
 
 class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=255, read_only=True)
     email = serializers.CharField(max_length=255)
     password = serializers.CharField(max_length=128, write_only=True)
     token = serializers.CharField(max_length=255, read_only=True)
 
-    def validate(self, data):
+    def create(self, data):
+        # The `validate` method is where we make sure that the current
+        # instance of `LoginSerializer` has "valid". In the case of logging a
+        # user in, this means validating that they've provided an email
+        # and password and that this combination matches one of the users in
+        # our database.
         email = data.get('email', None)
         password = data.get('password', None)
-        try:
-            user = authenticate(email=email, password=password)
-        except User.DoesNotExist:
+
+        # Raise an exception if an
+        # email is not provided.
+        if email is None:
             raise serializers.ValidationError(
-                'User with given email and password does not exists'
+                'An email address is required to sign in'
             )
+
+        # Raise an exception if an
+        # email is not provided.
+        if password is None:
+            raise serializers.ValidationError(
+                'A password required to sign in'
+            )
+
+        # we pass `email` as the `username` value since in our User
+        # model we set `USERNAME_FIELD` as `email`.
+        user = authenticate(username=email, password=password)
+
+        # If no user was found matching this email/password combination then
+        # `authenticate` will return `None`. Raise an exception in this case.
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with this email and password was not found.'
+            )
+
+        # we can also apply this
+        # try:
+        #     user = authenticate(email=email, password=password)
+        # except User.DoesNotExist:
+        #     raise serializers.ValidationError(
+        #         'User with given email and password does not exists'
+        #     )
 
         # if user in None:
         #     raise serializers.ValidationError(
         #         'A user with this email and password is not found.'
         #     )
         # try:
-        payload = JWT_PAYLOAD_HANDLER(user)
-        jwt_token = JWT_ENCODE_HANDLER(payload)
-        update_last_login(None, user)
+        # payload = JWT_PAYLOAD_HANDLER(user)
+        # jwt_token = JWT_ENCODE_HANDLER(payload)
+        # update_last_login(None, user)
         # except User.DoesNotExist:
         #     raise serializers.ValidationError(
         #         'User with given email and password does not exists'
         #     )
         return {
+            'username': user.username,
             'email': user.email,
-            'token': jwt_token
+            'token': user.token
         }
 
 

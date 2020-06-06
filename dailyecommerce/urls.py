ea71fd9fa395569dc3a2c3dbf5e2e9264860ff53
tanvir43 <raihncse43@gmail.com>
2020-06-06 19:32:38 +0600

    added dfr-yasg dependencies and urls

--- dailyecommerce/urls.py
+++ dailyecommerce/urls.py
@@ -28,6 +28,25 @@ from django.views.decorators.csrf import csrf_exempt
 
 # schema_view = get_swagger_view(title="dailyecommerce api's")
 
+from .yasg import urlpatterns as url_doc
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="DailyEcommerceAPI",
+      default_version='v1',
+      description="Test description",
+      # terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     # path('api-documentation', schema_view),
     path('admin/', admin.site.urls),
@@ -36,6 +55,11 @@ urlpatterns = [
     path('api/', include('account.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     url(r'^api-auth/', include('rest_framework.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += url_doc

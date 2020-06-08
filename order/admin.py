4f9606c41be9c4aeae9134a4060347804567e157
tanvir43 <raihncse43@gmail.com>
2020-06-08 21:48:41 +0600

    Added Order, Payment and Coupon model and registered them

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Order, Payment, Coupon
+
+admin.site.register(Order)
+admin.site.register(Payment)
+admin.site.register(Coupon)
+
 # Register your models here.
--- order/models.py
+++ order/models.py
@@ -1,6 +1,84 @@
+from uuid import uuid4
+
 from django.db import models
+
+from account.models import User, Address
+from products.models import OrderItem
+
+from commons.abstract import DateTimeModel
+from commons.choices import OrderStatus
 
 # Create your models here.
 
-class Order(models.Model):
-    pass
+
+class Order(DateTimeModel):
+    user = models.ForeignKey(User, related_name="orders", on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    status = models.CharField(
+        max_length=32, default=OrderStatus.DRAFT, choices=OrderStatus.CHOICES
+    )
+    currency = models.CharField(
+        max_length=30,
+        default="BDT",
+    )
+    shipping_address = models.ForeignKey(
+        Address, related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
+    billing_address = models.ForeignKey(
+        Address, related_name='billing_address', on_delete=models.SET_NULL, blank=True, null=True)
+    payment = models.ForeignKey(
+        'Payment', on_delete=models.SET_NULL, blank=True, null=True)
+    coupon = models.ForeignKey(
+        'Coupon', on_delete=models.SET_NULL, blank=True, null=True)
+    being_delivered = models.BooleanField(default=False)
+    received = models.BooleanField(default=False)
+    refund_requested = models.BooleanField(default=False)
+    refund_granted = models.BooleanField(default=False)
+    token = models.CharField(max_length=36, unique=True, blank=True, null=True)
+    # Token of a checkout instance that this order was created from
+    checkout_token = models.CharField(max_length=36, blank=True, null=True)
+
+    def __str__(self):
+        return self.status
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price
+        if self.coupon:
+            total -= self.coupon.amount
+        return total
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = str(uuid4())
+        return super(*args, **kwargs)
+
+
+class Payment(DateTimeModel):
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True)
+    payment_method = models.CharField(max_length=100)
+    payment_via = models.CharField(max_length=100, null=True, blank=True)
+    trxid = models.CharField(max_length=256, null=True, blank=True)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.payment_method
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=15) #Todo may the `code` should be unique
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.code
+
+
+class Refund(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    reason = models.TextField()
+    accepted = models.BooleanField(default=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.pk}"

1192a3108dd13df99b383df52ecdf969bda7dd61
tanvir43 <raihncse43@gmail.com>
2020-06-08 21:47:40 +0600

    modified Address model

--- account/models.py
+++ account/models.py
@@ -17,6 +17,8 @@ from django.forms.models import model_to_dict
 from django_countries.fields import Country, CountryField
 from phonenumber_field.modelfields import PhoneNumber, PhoneNumberField
 
+from commons.abstract import DateTimeModel
+
 
 class Role(models.Model):
     SUPER_ADMIN = 1
@@ -181,55 +183,58 @@ class User(AbstractUser):
         return token.decode('utf-8')
 
 
-class Address(models.Model):
-    first_name = models.CharField(max_length=256, blank=True)
-    last_name = models.CharField(max_length=256, blank=True)
-    company_name = models.CharField(max_length=256, blank=True)
-    street_address_1 = models.CharField(max_length=256, blank=True)
-    street_address_2 = models.CharField(max_length=256, blank=True)
-    city = models.CharField(max_length=256, blank=True)
-    city_area = models.CharField(max_length=128, blank=True)
-    postal_code = models.CharField(max_length=20, blank=True)
-    country = CountryField()
-    country_area = models.CharField(max_length=128, blank=True)
-    phone = PhoneNumberField(blank=True, default="")
+class Address(DateTimeModel):
+    user = models.ForeignKey(User, related_name='addresses', on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=256, blank=True, null=True)
+    address = models.TextField(max_length=300)
+    city = models.CharField(max_length=256)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+    country = CountryField(default="Bangladesh")
+    phone = PhoneNumberField(default="")
 
+    # first_name = models.CharField(max_length=256, blank=True, null=True)
+    # last_name = models.CharField(max_length=256, blank=True, null=True)
+    # apartment_address = models.CharField(max_length=100)
+    # street_address = models.CharField(max_length=100)
+    # street_address_1 = models.CharField(max_length=256, blank=True, null=True)
+    # street_address_2 = models.CharField(max_length=256, blank=True, null=True)
+    # address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+    # city_area = models.CharField(max_length=128, blank=True)
+    # country_area = models.CharField(max_length=128, blank=True, null=True)
     # objects = AddressQueryset.as_manager()
 
     class Meta:
         ordering = ("pk",)
 
-    @property
-    def full_name(self):
-        return "%s %s" % (self.first_name, self.last_name)
+    # @property
+    # def full_name(self):
+    #     return "%s %s" % (self.first_name, self.last_name)
 
     def __str__(self):
-        if self.company_name:
-            return "%s - %s" % (self.company_name, self.full_name)
-        return self.full_name
+        return self.user.name
 
-    def __eq__(self, other):
-        if not isinstance(other, Address):
-            return False
-        return self.as_data() == other.as_data()
+    # def __eq__(self, other):
+    #     if not isinstance(other, Address):
+    #         return False
+    #     return self.as_data() == other.as_data()
+    #
+    # __hash__ = models.Model.__hash__
 
-    __hash__ = models.Model.__hash__
-
-    def as_data(self):
-        """Return the address as a dict suitable for passing as kwargs.
-
-        Result does not contain the primary key or an associated user.
-        """
-        data = model_to_dict(self, exclude=["id", "user"])
-        if isinstance(data["country"], Country):
-            data["country"] = data["country"].code
-        if isinstance(data["phone"], PhoneNumber):
-            data["phone"] = data["phone"].as_e164
-        return data
-
-    def get_copy(self):
-        """Return a new instance of the same address."""
-        return Address.objects.create(**self.as_data())
+    # def as_data(self):
+    #     """Return the address as a dict suitable for passing as kwargs.
+    #
+    #     Result does not contain the primary key or an associated user.
+    #     """
+    #     data = model_to_dict(self, exclude=["id", "user"])
+    #     if isinstance(data["country"], Country):
+    #         data["country"] = data["country"].code
+    #     if isinstance(data["phone"], PhoneNumber):
+    #         data["phone"] = data["phone"].as_e164
+    #     return data
+    #
+    # def get_copy(self):
+    #     """Return a new instance of the same address."""
+    #     return Address.objects.create(**self.as_data())
 
 
 # class UserProfile(models.Model):

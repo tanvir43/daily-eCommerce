908acf783fd2bb58ba55dc9ac039f0562f48a99a
tanvir43 <raihncse43@gmail.com>
2020-06-18 09:06:56 +0600

    register orderitem model in admin

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Category, Product, Unit
+from .models import Category, Product, Unit, OrderItem
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Unit)
+admin.site.register(OrderItem)

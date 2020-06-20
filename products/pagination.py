d5d80dc4c6dbe13a696fa38bbc11c6975481d05a
tanvir43 <raihncse43@gmail.com>
2020-06-20 09:44:49 +0600

    override page out of range pagination error

--- products/pagination.py
+++ products/pagination.py
@@ -1,7 +1,18 @@
+import json
+from rest_framework.response import Response
+# from rest_framework.exceptions import NotFound
 from rest_framework.pagination import (
     LimitOffsetPagination,
     PageNumberPagination,
     )
+from rest_framework.exceptions import APIException
+from rest_framework import status
+
+
+class NotFound(APIException):
+    status_code = status.HTTP_200_OK
+    default_detail = ('Not found.')
+    default_code = 'not_found'
 
 
 class ProductLimitOffsetPagination(LimitOffsetPagination):
@@ -11,3 +22,58 @@ class ProductLimitOffsetPagination(LimitOffsetPagination):
 
 class ProductPageNumberPagination(PageNumberPagination):
     page_size = 10
+    page_size_query_param = 'page_size'
+
+    def paginate_queryset(self, queryset, request, view=None):
+        """
+        Paginate a queryset if required, either returning a
+        page object, or `None` if pagination is not configured for this view.
+        """
+        page_size = self.get_page_size(request)
+        if not page_size:
+            return None
+
+        paginator = self.django_paginator_class(queryset, page_size)
+        page_number = request.query_params.get(self.page_query_param, 1)
+        if page_number in self.last_page_strings:
+            page_number = paginator.num_pages
+
+        try:
+            self.page = paginator.page(page_number)
+        except Exception as exc:
+            # msg = self.invalid_page_message.format(
+            #     page_number=page_number, message=str(exc)
+            # )
+            msg = {
+                'next': None,
+                'previous': None,
+                'count': 0,
+                'limit': 0,
+                'results': []
+            }
+            raise NotFound(msg)
+
+        if paginator.num_pages > 1 and self.template is not None:
+            # The browsable API should display pagination controls.
+            self.display_page_controls = True
+
+        self.request = request
+        return list(self.page)
+
+    def get_paginated_response(self, data):
+        if len(data) > 0:
+            return Response({
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link(),
+                'count': self.page.paginator.count,
+                'limit': self.page_size,
+                'results': data
+            })
+        else:
+            return Response({
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link(),
+                'count': self.page.paginator.count,
+                'limit': self.page_size,
+                'results': []
+            })

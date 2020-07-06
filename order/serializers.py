456a921d62c0961e72ba3f99da009320b06b18f0
tanvir43 <raihncse43@gmail.com>
2020-07-06 21:03:49 +0600

    modified order serializer as associated order item serializers added

--- order/serializers.py
+++ order/serializers.py
@@ -6,10 +6,38 @@ from rest_framework.serializers import (
     PrimaryKeyRelatedField
     )
 
+from account.serializers import AddressSerializer
+from products.serializers import OrderItemSerializer
+
 from .models import Order
 
 
 class OrderSerializer(ModelSerializer):
+    shipping_address = AddressSerializer()
+    billing_address = AddressSerializer()
+    email = SerializerMethodField()
+    order_items = OrderItemSerializer(many=True, read_only=True)
+    total_amount = serializers.SerializerMethodField()
+
+    def get_total_amount(self, obj):
+        return obj.get_total()
+
+    def get_email(self, obj):
+        return obj.user.email
+
     class Meta:
         model = Order
-        fields = "__all__"
+        fields = (
+            'id',
+            'created_at',
+            'updated_at',
+            "status",
+            "currency",
+            "being_delivered",
+            "received",
+            "shipping_address",
+            "billing_address",
+            "email",
+            "order_items",
+            "total_amount"
+        )

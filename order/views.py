b25be631dd27111e2acd36512d63fd26dd44ada9
tanvir43 <raihncse43@gmail.com>
2020-08-06 12:47:22 +0600

    modified create order api

--- order/views.py
+++ order/views.py
@@ -23,7 +23,13 @@ from django_filters.rest_framework import DjangoFilterBackend
 class OrderListAPIView(ListAPIView):
     queryset = Order.objects.all().order_by('-created_at')
     serializer_class = OrderSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        order = Order.objects.filter(user=user, deleted=False).order_by('-created_at')
+        serializer = self.serializer_class(order, many=True)
+        return Response(serializer.data)
 
 
 class OrderCreateAPIView(CreateAPIView):
@@ -48,13 +54,14 @@ class OrderCreateAPIView(CreateAPIView):
                 user=user,
                 shipping_address=address,
                 billing_address=address,
-                currency="BDT"
+                currency="BDT",
+                total_amount=order_data['total_amount']
             )
-            order.save()
         # serializer = self.serializer_class(data=order_data)
         # serializer.is_valid(raise_exception=True)
         # serializer.save(user=request.user)
-            if len(data['items']) > 1:
+            print("item len", )
+            if len(data['items']) >= 1:
                 for item in data['items']:
                     product = Product.objects.get(slug=item['slug'])
                     if product:
@@ -66,10 +73,10 @@ class OrderCreateAPIView(CreateAPIView):
                                     ordered=True,
                                     quantity=item['quantity'],
                                 )
-                                product.stock -= item['quantity']
-                                product.save()
-                                order_item.save()
-                                order_item.order.add(order)
+                                # product.stock -= item['quantity']
+                                # product.save()
+                                # order_item.save()
+                                # order_item.order.add(order)
                             else:
                                 order_item = OrderItem(
                                     user=user,
@@ -77,10 +84,11 @@ class OrderCreateAPIView(CreateAPIView):
                                     ordered=True,
                                     quantity=1,
                                 )
-                                product.stock -= item['quantity']
-                                product.save()
-                                order_item.save()
-                                order_item.order.add(order)
+                            product.stock -= item['quantity']
+                            product.save()
+                            order.save()
+                            order_item.save()
+                            order_item.order.add(order)
                 return Response({"status": "order successfully placed"}, status=201)
             else:
                 return Response({"error": "Please select an item"}, status=400)

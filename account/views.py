905743afe81e97982aa6d8cb2f5ee3a829743901
tanvir43 <raihncse43@gmail.com>
2020-08-14 12:45:34 +0600

    modified user sign up and login api for user activation

--- account/views.py
+++ account/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import Group
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.http import HttpResponse
 
 from rest_framework.generics import (
     CreateAPIView,
@@ -25,6 +30,8 @@ from account.serializers import (
     AddressSerializer
     )
 
+from .tokens import account_activation_token
+
 
 class UserLoginAPIView(CreateAPIView):
     queryset = User.objects.all()
@@ -36,9 +43,14 @@ class UserLoginAPIView(CreateAPIView):
         serializer = self.serializer_class(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
+        print("serialized data", serializer.data)
+        if serializer.data['approved']:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            error = {
+                'error':  'Please activate your account'
+            }
+            return Response(error, status=status.HTTP_200_OK)
     # def post(self, request):
     #     serializer = self.serializer_class(data=request.data)
     #     serializer.is_valid(raise_exception=True)
@@ -52,7 +64,7 @@ class UserLoginAPIView(CreateAPIView):
     #     return Response(response, status=status_code)
 
 
-class UserRegistrationAPIView(APIView):
+class UserRegistrationAPIView(CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = UserRegistrationSerializer
 
@@ -61,7 +73,23 @@ class UserRegistrationAPIView(APIView):
         data = request.data
         serializer = self.serializer_class(data=data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        user = serializer.save()
+        print(user)
+        current_site = get_current_site(request)
+        mail_subject = 'Activate your account.'
+        message = render_to_string('account_active_email.html', {
+            'user': user,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': account_activation_token.make_token(user),
+        })
+        print("Message", message)
+        print("user pk", user.pk)
+        to_email = data.get('email')
+        email = EmailMessage(
+            mail_subject, message, to=[to_email]
+        )
+        email.send()
         status_code = status.HTTP_201_CREATED
         # response = {
         #     'success': 'True',
@@ -171,7 +199,7 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request, user)
+        # login(request, user)
         # return redirect('home')
         return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
     else:

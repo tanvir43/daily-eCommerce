b1b4f98090564611eb550212dc97a439331cbd49
tanvir43 <raihncse43@gmail.com>
2020-09-13 10:28:25 +0600

    modified apps configuration for elasticsearch-DSL

--- products/apps.py
+++ products/apps.py
@@ -1,5 +1,23 @@
 from django.apps import AppConfig
+from django.conf import settings
+
+from elasticsearch_dsl import connections
 
 
 class ProductsConfig(AppConfig):
     name = 'products'
+
+    def ready(self):
+        import products.signals
+        try:
+            connections.create_connection(
+                # 'art',
+                hosts=[
+                    {
+                     'host': 'localhost',
+                     'port': '9200'
+                    }
+                ]
+            )
+        except Exception as e:
+            print(e)

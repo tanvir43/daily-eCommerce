a0033167b14dff4b663809e69c2fc771affef70e
tanvir43 <raihncse43@gmail.com>
2020-09-22 11:04:05 +0600

    added created_by column in order model

--- order/models.py
+++ order/models.py
@@ -52,10 +52,15 @@ class Order(DateTimeModel):
                                    related_name='order_updated',
                                    null=True,
                                    blank=True)
-    delivered_by = models.ForeignKey(User,
+    created_by = models.ForeignKey(User,
                                    on_delete=models.CASCADE,
+                                   related_name='order_created',
                                    null=True,
                                    blank=True)
+    delivered_by = models.ForeignKey(User,
+                                     on_delete=models.CASCADE,
+                                     null=True,
+                                     blank=True)
 
     def __str__(self):
         return f"Status: {self.status}, ID: {self.pk}"
